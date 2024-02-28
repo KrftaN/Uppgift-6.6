b6e3f40105a7af802bb2c0319eeae0cc1b59d085
Admin <Admin@TEB127118>
2024-02-28 10:35:54 +0100

    Add project files.

---
+++ Uppgift 6.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_6._6
+{
+    class Program
+    {
+        static string StoraOchSmåBokstäver(string sträng)
+        {
+            string[] deladSträng = sträng.Split(" ");
+
+            for (int i = 0; i < deladSträng.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    deladSträng[i] = deladSträng[i].ToUpper();
+                }
+                else
+                {
+                    deladSträng[i] = deladSträng[i].ToLower();
+                }
+            }
+            return string.Join(" ", deladSträng);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StoraOchSmåBokstäver("Detta är ett exempel och ett test snälla fungera"));
+        }
+    }
+}
